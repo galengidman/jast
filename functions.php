67692f88319dec0989f7bde782be1257b56b10c6
Galen Gidman <galen@galengidman.com>
2012-11-27 10:20:01 -0600

    re-organized and documented functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php
 
-
+/**
+ * Get things setup.
+ */
 function jast_setup() {
-	
-	// register the theme's sidebar
+
+	// register the sidebar
 	register_sidebar( array(
 		'name' => 'Sidebar',
 		'id' => 'sidebar',
@@ -13,90 +15,39 @@ function jast_setup() {
 		'before_title' => '<h2>',
 		'after_title' => '</h2>'
 	) );
-	
-	// get rid of the annoying default [gallery] shortcode styles
+
+	// get rid of the default [gallery] shortcode styles
 	add_filter( 'use_default_gallery_style', '__return_false' );
 
 	// load the threaded comment scripts if needed
 	function jast_comments_scripts() {
 		if ( is_singular() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );		
+			wp_enqueue_script( 'comment-reply' );
 		}
 	}
-
 	add_action( 'wp_head', 'jast_comments_scripts' );
-	
+
 }
 
 add_action( 'init', 'jast_setup' );
 
 
-
-// context-sensitive title
-// change the value of $sep to whatever you like
-
+/**
+ * Context-sensitive <title> with customizable separator.
+ */
 function jast_title( $sep = '|' ) {
-	
-	// first off, add the title
 	wp_title( $sep, true, 'right' );
-
-	// then the blog name
 	bloginfo( 'name' );
-
-	// and add the blog description for front/homepage
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) ) {
-		echo " $sep $site_description";		
-	}
-
-}
-
-
-
-// function for created the published on links
-
-function jast_published_on() {
-	echo 'Published on <a href="' . get_permalink() . '">' . get_the_time( 'F j, Y' ) . '</a>';
-}
-
-
-// the post body
-// $format defaults to 'content' but also accepts 'excerpt'
-function jast_entry_body( $format = 'content' ) {
-	if ( $format == 'excerpt' ) {
-		the_excerpt();
-	} else {
-		the_content( 'Continue reading &rarr;' );
-		wp_link_pages();
+		echo " $sep $site_description";
 	}
 }
 
 
-// function for the post meta
-
-function jast_post_meta() {
-	
-	if ( 'post' == get_post_type() ) {
-		// tags
-		the_tags( 'Tagged ', ', ', ' | ' );
-
-		// categories
-		echo 'Categorized ';
-		the_category( ', ' );
-	}
-		
-	// comments
-	if ( comments_open() ) {
-		echo ' | <a href="' . get_comments_link() . '">';
-		comments_number( 'No Replies', 'One Reply', '% Replies' );
-		echo '</a>';
-	}
-	
-}
-
-
-// context-sensitive title for archive pages
-
+/**
+ * Creates context-sensitive page titles for archive pages.
+ */
 function jast_archive_title() {
 	if ( is_category() ) {
 		echo 'Posts categorized ';
@@ -116,6 +67,47 @@ function jast_archive_title() {
 }
 
 
+/**
+ * Easy way to handle theme-wide "published on" date crap.
+ */
+function jast_published_on() {
+	echo 'Published on <a href="' . get_permalink() . '">' . get_the_time( 'F j, Y' ) . '</a>';
+}
+
+
+/**
+ * Provides an easy way display the_excerpt and the_content in templates.
+ */
+function jast_entry_body( $format ) {
+	if ( $format == 'excerpt' ) {
+		the_excerpt();
+	} else {
+		the_content( 'Continue reading &rarr;' );
+		wp_link_pages();
+	}
+}
+
+/**
+ * Clean way to handle theme-wide post meta.
+ */
+function jast_post_meta() {
+	if ( 'post' == get_post_type() ) {
+		the_tags( 'Tagged ', ', ', ' | ' );
+		echo 'Categorized ';
+		the_category( ', ' );
+	}
+
+	if ( comments_open() ) {
+		echo ' | <a href="' . get_comments_link() . '">';
+		comments_number( 'No Replies', 'One Reply', '% Replies' );
+		echo '</a>';
+	}
+}
+
+
+/**
+ * Function for theme-wide pagination.
+ */
 function jast_pagination() {
 	posts_nav_link( ' | ', '&larr; Newer', 'Older &rarr;' );
 }
