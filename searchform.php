22bcc43f1fdc2636dc2ca16f5166f7040a78ab15
Galen Gidman <galen@galengidman.com>
2012-12-13 14:31:37 -0600

    fixed searchbox placeholder text

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
-	<input type="search" class="search-box" name="s" placeholder="search for&ellipse;">
+	<input type="search" class="search-box" name="s" placeholder="search">
 	<input type="submit" class="search-submit" value="Search">
 </form>
