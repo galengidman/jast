4d09605a2cbeb7292133f56a4aef7ca3b4b52aa9
Galen Gidman <galen@galengidman.com>
2018-02-14 11:41:57 -0600

    PHP formatting

--- functions.php
+++ functions.php
@@ -6,28 +6,22 @@ define( 'JAST_ASSET_VER', 1 );
 include get_template_directory() . '/includes/template-tags.php';
 
 add_action( 'after_setup_theme', function() {
-
 	// Replace 500 with whatever the width of your main content column
 	$GLOBALS['content_width'] = 500;
 
 	load_theme_textdomain( 'jast' );
 
 	add_theme_support( 'automatic-feed-links' );
-
 	add_theme_support( 'post-thumbnails' );
-
 	add_theme_support( 'title-tag' );
-
 	add_theme_support( 'html5' );
 
 	register_nav_menus( [
 		'main' => esc_html__( 'Main Menu', 'jast' ),
 	] );
-
 } );
 
 add_action( 'wp_enqueue_scripts', function() {
-
 	wp_enqueue_style(
 		'jast',
 		esc_url( get_template_directory_uri() . '/assets/css/jast.css' ),
@@ -42,11 +36,9 @@ add_action( 'wp_enqueue_scripts', function() {
 		JAST_ASSET_VER,
 		true // Output in footer for better performance
 	);
-
 } );
 
 add_action( 'widgets_init', function() {
-
 	$markup = [
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
@@ -58,5 +50,4 @@ add_action( 'widgets_init', function() {
 		'name' => esc_html__( 'Sidebar', 'jast' ),
 		'id'   => 'sidebar',
 	] ) );
-
 } );
--- includes/template-tags.php
+++ includes/template-tags.php
@@ -1,21 +1,15 @@
 <?php
 
 function jast_nothing_found() {
-
 	return esc_html__( 'Nothing found.', 'jast' );
-
 }
 
 function jast_entry_meta() {
-
 	?>
-
 	<time datetime="<?php echo esc_attr( get_the_date( 'c' ) ); ?>"><?php echo get_the_date(); ?></time>
 	<span class="sep"></span>
 	<?php the_author_posts_link(); ?>
 	<span class="sep"></span>
 	<a href="<?php echo esc_url( get_comments_link() ); ?>"><?php comments_number(); ?></a>
-
 	<?php
-
 }
